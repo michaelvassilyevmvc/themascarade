ae693765e9f1942e46e78b6479b5d6c66add5352
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-08-25 12:30:42 +0500

    Настройка Rental согласно стандартам DDD

--- vassilyev.themascarade.Domain/Rentals/Rental.cs
+++ vassilyev.themascarade.Domain/Rentals/Rental.cs
@@ -2,18 +2,30 @@
 
 namespace vassilyev.themascarade.Domain.Rentals;
 
-public sealed class Rental: Entity
+public sealed class Rental : Entity
 {
-    public Rental(Guid id):base(id)
+    public Rental(Guid id, Guid costumeId, Guid customerId, RentalStatus status, DateOnly start, DateOnly end,
+        Money price) : base(id)
     {
-        
+        CostumeId = costumeId;
+        CustomerId = customerId;
+        Status = status;
+        StartDate = start;
+        EndDate = end;
+        Price = price;
     }
-    
-    public Guid CostumeId { get; init; }     // FK -> Costume
-    public Guid CustomerId { get; init; }    // FK -> Customer
+
+    public Guid CostumeId { get; init; } // FK -> Costume
+    public Guid CustomerId { get; init; } // FK -> Customer
     public RentalStatus Status { get; private set; } = RentalStatus.Reserved;
     public DateOnly StartDate { get; private set; }
-    public DateOnly EndDate { get; private set; }       // End > Start
-    public decimal TotalAmount { get; private set; }    // >= 0
-    public string Currency { get; private set; } = "KZT"; // ISO 4217 (3 буквы)
+    public DateOnly EndDate { get; private set; } // End > Start
+    public Money Price { get; private set; }
+
+    public static Rental Create(Guid costumeId, Guid customerId, RentalStatus status, DateOnly start, DateOnly end,
+        Money price)
+    {
+        var rental = new Rental(Guid.NewGuid(), costumeId, customerId, status, start, end, price);
+        return rental;
+    }
 }
